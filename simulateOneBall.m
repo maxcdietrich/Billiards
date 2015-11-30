4a03b11a53f14ec06c40c5ce00f3ec31e8d22adf
maxcdietrich <maxcdietrich@gmail.com>
2015-11-30 12:38:57 -0500

    changed code to do euler's method

--- simulateOneBall.m
+++ simulateOneBall.m
@@ -10,63 +10,73 @@ function timeSeries = simulateOneBall(initial_x, initial_y, initial_v, angle)
     %chosen Values
     initial_time = 0; %seconds
     final_time = 60; %seconds
+    time_step = 0.01; %seconds
     
     %caluclated values
     initial_vx = initial_v * cosd(angle);
     initial_vy = initial_v * sind(angle);
     
-    %ode45
-    % higher values of refine calculates at more points
-    refine = 4;
-    options = odeset('Events', @events, 'Refine', refine);
+
+    Time_all = (initial_time: time_step: final_time);
+    X_all = zeros(1, length(Time_all));
+    Y_all = zeros(1, length(Time_all));
+    Vx_all = zeros(1, length(Time_all));
+    Vy_all = zeros(1, length(Time_all));
+    X_all(1) = initial_x;
+    Y_all(1) = initial_y;
+    Vx_all(1) = initial_vx;
+    Vy_all(1) = initial_vy;
     
-    Time_all = [];
-    X_all = [];
-    Y_all = [];
-    Vx_all = [];
-    Vy_all = [];
         
-    for i = 1:100
-        [Time, Results] = ode45(@netFlows, [initial_time; final_time], [initial_x; initial_y; initial_vx; initial_vy], options);
-        X = Results(:,1);
-        Y = Results(:,2);
-        Vx = Results(:,3);
-        Vy = Results(:,4);
+    for t = 2:length(Time_all)
         
-        Time_all = [Time_all; Time];
-        X_all = [X_all; X];
-        Y_all = [Y_all; Y];
-        Vx_all = [Vx_all; Vx];
-        Vy_all = [Vy_all; Vy];
+        initial_vx = Vx_all(t);
+        initial_vy = Vy_all(t);
         
-        xEnd = X(end);
-        yEnd = Y(end);
-        vxEnd = Vx(end);
-        vyEnd = Vy(end);
-        tEnd = Time(end);
+        %Friction
+        frictionForce = -rollingFrictionCoefficient * normalForce;
+        force_x = frictionForce * x_component(initial_vx, initial_vy);
+        force_y = frictionForce * y_component(initial_vx, initial_vy);
         
-        initial_x = xEnd;
-        initial_y = yEnd;
-        initial_vx = vxEnd;
-        initial_vy = vyEnd;
-        initial_time = tEnd;
         
-              
+        %netFlows
+        deltaX = initial_vx;
+        deltaY = initial_vy;
+        deltaVx = force_x/ballMass;
+        deltaVy = force_y/ballMass;
+        
+        previous_x = X_all(t-1);
+        previous_y = Y_all(t-1);
+        previous_Vx = Vx_all(t-1);
+        previous_Vy = Vy_all(t-1);
+        
+        x = previous_x + deltaX * time_step;
+        y = previous_y + deltaY * time_step;
+        vx = previous_Vx + deltaVx * time_step;
+        vy = previous_Vy + deltaVy * time_step;     
+         
+        
         if (hasBallStopped(vx, vy))
             break
         end
-        if (isHittingTopWall(xEnd, yEnd, vxEnd, vyEnd, ball_radius))
-            initial_vy = hitHorizontalWall(vyEnd);
+        if (isHittingTopWall(x, y, vx, vy, ball_radius))
+            vy = hitHorizontalWall(vy);
         end
-        if (isHittingBottomWall(xEnd, yEnd, vxEnd, vyEnd, ball_radius))
-            initial_vy = hitHorizontalWall(vyEnd);
+        if (isHittingBottomWall(x, y, vx, vy, ball_radius))
+            vy = hitHorizontalWall(vy);
         end
-        if (isHittingLeftWall(xEnd, yEnd, vxEnd, vyEnd, ball_radius))
-            initial_vx = hitVerticalWall(vxEnd);
+        if (isHittingLeftWall(x, y, vx, vy, ball_radius))
+            vx = hitVerticalWall(vx);
         end
-        if (isHittingRightWall(xEnd, yEnd, vxEnd, vyEnd, ball_radius))
-            initial_vx = hitVerticalWall(vxEnd);
+        if (isHittingRightWall(x, y, vx, vy, ball_radius))
+            vx = hitVerticalWall(vx);
         end
+        
+        X_all(t) = x;
+        Y_all(t) = y;
+        Vx_all(t) = vx;
+        Vy_all(t) = vy;
+        
     end    
        
     timeSeries = [Time_all, X_all, Y_all, Vx_all, Vy_all];
